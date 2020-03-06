1391ff9510b47e7b0d762328c0b885209e451f73
Penny Tague <pmtague9@gmail.com>
2020-03-05 17:59:43 -0800

    Fixing env variables

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -90,13 +90,20 @@ WSGI_APPLICATION = 'adv_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     },
+# 	'production': {
+# 		'ENGINE': 'django.db.backends.postgresql',
+# 		'NAME': config('DATABASE_URL')
+# 	}
+# }
+import dj_database_url 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=config('DATABASE_URL'), conn_max_age=600)
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
